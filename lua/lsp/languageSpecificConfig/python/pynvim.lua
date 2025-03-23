259b8d62f3ccafc0fdc9ea2e1146f4a843c5a4e0
kayinsg <kayin.gayle@gmail.com>
2025-03-23 15:38:59 -0400

    squash

--- lua/lsp/languageSpecificConfig/python/pynvim.lua
+++
@@ -1,3 +0,0 @@
-vim.g.python3_host_prog = "/home/kayinfire/projects/python/.venv/bin/python3.11"
-vim.g.python3_host_prog = os.getenv("HOME") .. "/home/kayinfire/projects/python/.venv/bin/black"
-vim.g.python3_host_prog = '/home/kayinfire/projects/python/.venv/bin/python'
